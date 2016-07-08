4fab96d9c607f99c512f4d01fadcf675bcc20926
ciou <yyouko22@gmail.com>
2016-07-08 18:42:42 +0800

    figaro管理機密資訊

--- config/initializers/pay2go.rb
+++ config/initializers/pay2go.rb
@@ -1,15 +1,7 @@
 Pay2go.setup do |pay2go|
-  if Rails.env.development?
-    pay2go.merchant_id = "12198269"
-
-    pay2go.hash_key    = "vyhqJC01Fxy8NGhJ99mayMJc7HWtg44L"
-    pay2go.hash_iv     = "CmcmZcnEJWNwC4fM"
-    pay2go.service_url = "https://capi.pay2go.com/MPG/mpg_gateway"
-  else
-    pay2go.merchant_id = ""
-
-    pay2go.hash_key    = ""
-    pay2go.hash_iv     = ""
-    pay2go.service_url = "https://api.pay2go.com/MPG/mpg_gateway"
-  end
+  # 已經在figaro 做完判斷 development or production
+  pay2go.merchant_id = ENV['pay2go_merchant_id']
+  pay2go.hash_key    = ENV['pay2go_hash_key']
+  pay2go.hash_iv     = ENV['pay2go_hash_iv']
+  pay2go.service_url = ENV['pay2go_service_url']
 end
